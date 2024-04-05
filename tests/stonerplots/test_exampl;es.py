02bd88a7c651b2d49a7112f81f6ec9a0f51e2e38
Gavin Burnell <g.burnell@leeds.ac.uk>
2024-04-05 19:05:11 +0100

    Add pytest action

--- tests/stonerplots/test_exampl;es.py
+++ tests/stonerplots/test_exampl;es.py
@@ -3,18 +3,26 @@
 import pathlib
 import runpy
 import pytest
-import stonerplots
 import sys
 
-scriptpath = pathlib.Path(stonerplots.__file__).parent.parent.parent/"examples"/"plot_examples"
-scripts = [x for x in scriptpath.resolve().glob('*.py') if not x.stem.startswith("_")]
+srcpath = pathlib.Path(__file__).parent.parent.parent / "src"
+srcpath = str(srcpath.absolute())
+if srcpath not in sys.path:
+    sys.path.insert(0, srcpath)
+
+import stonerplots
+
+scriptpath = pathlib.Path(stonerplots.__file__).parent.parent.parent / "examples" / "plot_examples"
+scripts = [x for x in scriptpath.resolve().glob("*.py") if not x.stem.startswith("_")]
 
 if str(scriptpath) not in sys.path:
     sys.path.insert(0, str(scriptpath))
 
-@pytest.mark.parametrize('script', scripts)
+
+@pytest.mark.parametrize("script", scripts)
 def test_script_execution(script):
     runpy.run_path(script)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     pytest.main(["--pdb", __file__])
